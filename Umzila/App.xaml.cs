f77e51b5b13be136a219df600db64075e86f13a4
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-05-07 15:37:02 +0200

    Add project files.

---
+++ Umzila/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Umzila
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Umzila/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Umzila
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
